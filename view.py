4debc34e14b6a8bd80e858edcfd9eaccbf654543
29Tatiana <amiravo@mail.ru>
2023-01-02 17:53:32 +0300

    view

---
+++ view.py
@@ -0,0 +1,37 @@
+import validate
+
+def main_menu():
+    print('Главное меню')
+    mode = input('1. Записать данные в книгу\n2. Показать всю книгу\n3. Поиск записи\n4. Выход\nВведите 1, 2, 3 и 4: ')
+    if mode in '1234':
+        return int(mode)
+    else:
+        print('Ошибка! Введите 1, 2, 3 или 4!')
+
+def write_data():
+    phone_record = [input('Введите имя: ').title(), input('Введите фамилию: ').title()]
+    phone = ''
+    while not validate.check_phone(phone):
+        phone = input('Введите номер телефона: ')
+        if not validate.check_phone(phone):
+            print('Телефон введен неверно. Попробуйте снова.')
+    phone_record.append(phone)
+    description = input('Введите описание: ')
+    if description:
+        phone_record.append(description)
+    print(f'Запись внесена в книгу: {str.join(", ", phone_record)}')
+    return phone_record
+
+def search_data():
+    return input('Введите текст или номер для поиска: ')   
+
+def show_data(data):
+    print(f'Найдено записей: {len(data)}')
+    for line in data:
+        print(line)
+
+def show_ext_data(data):
+    print(f'Найдено записей: {len(data)}\n')
+    for line in data:
+        for text in line:
+            print(text)             
